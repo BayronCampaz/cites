210bc98a5d882c32284948b738b4f5047096c61a
BayronCampaz <bayrondaymir@yahoo.es>
2020-12-18 15:41:20 -0500

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from 'react';
+import Form from './components/Forms'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <h1>Patient Manager</h1>
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-6 col-md-6 col-sm-12">
+            <Form/>
+          </div>
+          <div className="col-lg-6 col-md-6 col-sm-12">
+            Column 2  
+          </div>
+        </div>
+      </div>
+    </Fragment>
+
   );
 }
 
